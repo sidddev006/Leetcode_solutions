b16d07536a5247ceeb6c55144cc93df6351c30e0
Siddharth Mishra <sidd.dev006@gmail.com>
2026-07-29 15:55:08 +0530

    Time: 0 ms (100%), Space: 9.5 MB (99.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(), s.end());
-       sort(t.begin(), t.end());
-       return s == t;
+        int freq[26] = {0};
+        int n1 = s.length();
+        int n2 = t.length();
+        if(n1 != n2) return false;
+        for(int i = 0;i<n1;i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+        for(int i = 0;i<26;i++){
+            if(freq[i] != 0) return false;
+        }
+        return true;
     }
 };
