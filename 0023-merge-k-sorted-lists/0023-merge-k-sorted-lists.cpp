aa7fc691c65acccbde512b97e811223f4ff9e408
Siddharth Mishra <sidd.dev006@gmail.com>
2026-08-20 00:15:30 +0530

    Time: 95 ms (13.79%), Space: 18.4 MB (82.71%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* headm = new ListNode(0);
+        ListNode* merge = headm;
+        ListNode* temp1 = list1;
+        ListNode* temp2 = list2;
+        while(temp1 && temp2){
+            if(temp1->val <= temp2->val) {
+                merge->next = temp1;
+                temp1 = temp1->next;
+                merge= merge->next;
+            }
+            else{
+                merge->next = temp2;
+                temp2 = temp2->next;
+                merge= merge->next;
+            }
+        }
+        /* This can be shortened by writing
+        while(temp1){
+            merge->next = temp1;
+            temp1 = temp1->next;
+            merge= merge->next;
+        }
+        while(temp2){
+            merge->next = temp2;
+            temp2 = temp2->next;
+            merge= merge->next;
+        }
+        */
+        merge->next = temp1 ? temp1 : temp2; // this works as either of them will be null after the initial while loop that i am running.
+        return headm->next;//This should be headm->next as headm contains a value 0 which will disrupt the entire merged linked list
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int n = lists.size();
+        if(n == 0) return NULL;
+        ListNode* merged = lists[0];
+        for(int i = 1;i<n;i++){
+            merged = mergeTwoLists(merged, lists[i]);
+        }
+        return merged;
+    }
+};
