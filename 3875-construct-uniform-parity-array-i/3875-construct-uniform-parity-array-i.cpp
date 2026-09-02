597007b7ec3cc20f2aba9ea484179443f5c2f4e3
Siddharth Mishra <sidd.dev006@gmail.com>
2026-09-02 10:01:33 +0530

    Time: 0 ms (100%), Space: 30.4 MB (36.43%) - LeetHub

---
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+/*
+construct another array of length n of which the nums2 should be either all odd or all even
+nums2[i] = nums1[i]
+nums2[i] = nums1[i] - nums1[j]
+*/
+    bool uniformArray(vector<int>& nums1) {
+        /*int n = nums1.size();
+        vector<int> nums2(n);
+        bool parity = false;
+        for(int i = 0; i<n;i++){
+
+        }*/
+        return true;
+    }
+};
