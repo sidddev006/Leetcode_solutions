a9b05360d307f0082af0d3c15b6a0bf0079106f2
Siddharth Mishra <sidd.dev006@gmail.com>
2026-09-03 08:39:50 +0530

    Time: 0 ms (100%), Space: 165.9 MB (62.83%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        int mn = nums1[0];
+        bool hasOdd = false;
+        for(int v: nums1){
+            if(v < mn){
+                mn = v;
+            }
+            if(v % 2 != 0) hasOdd = true;
+        }
+        if(mn % 2 != 0) return true;
+        return !hasOdd;
+    }
+};
